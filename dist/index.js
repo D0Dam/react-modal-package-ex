8e3dd576d8ca5b23cf6bdae8961b0fe56e0af393
minjaeKim <smallkdb@gmail.com>
2023-05-02 13:48:14 +0900

    chore: first setting

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MyModal from './Modal';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( /*#__PURE__*/React.createElement(MyModal, {
+  trigger: /*#__PURE__*/React.createElement("button", null, "\uC5F4\uAE30")
+}, /*#__PURE__*/React.createElement("h2", null, "\uC81C\uBAA9"), /*#__PURE__*/React.createElement("p", null, "\uB0B4\uC6A9")));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import MyModal from './Modal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MyModal trigger={<button>열기</button>}>
+        <h2>제목</h2>
+        <p>내용</p>
+      </MyModal>
     </div>
   );
 }
